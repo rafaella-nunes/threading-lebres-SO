f8bdaaa62c65c9d4fdb41076b25c7ab12be4b821
Rafaella <faellanunes01@gmail.com>
2023-08-12 21:20:47 -0300

    chore, bug: changes implementations

--- Corrida.java
+++ Corrida.java
@@ -1,39 +1,35 @@
 package lebres;
 
-public class Corrida {
+public class Corrida implements Runnable {
 
-	public static void main(String[] args) {
+	Lebre lebres;
+	Thread t;
 
-		Lebre querida = new Lebre("Querida", 500);
-		Thread t1 = new Thread(querida);
-		t1.start();
-		Lebre fofa = new Lebre("fofa", 1000);
-		Thread t2 = new Thread(fofa);
-		t2.start();
-		Lebre vandinha = new Lebre("vandinha", 800);
-		Thread t3 = new Thread(vandinha);
-		t3.start();
-		Lebre soninho = new Lebre("soninho", 1200);
-		Thread t4 = new Thread(soninho);
-		t4.start();
-		Lebre dunga = new Lebre("dunga", 200);
-		Thread t5 = new Thread(dunga);
-		t5.start();
+	final int num = 20;
+
+	public Corrida(String nome, Lebre lebre) {
+		this.lebres = lebre;
+		t = new Thread(this, nome);
+		t.start();
+	}
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
 		
-		try {
-			t1.join();
-			t2.join();
-			t3.join();
-			t4.join();
-			t5.join();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if (t.getName().equalsIgnoreCase("Lebre")){
+			for (int i=0; i<num; i++){
+				lebres.lebre1(true);
+			}
+			lebres.lebre1(false);
+		} else {
+			for (int i=0; i<num; i++){
+				lebres.lebre2(true);
+			}
+			lebres.lebre2(false);
 		}
-		System.out.println("A corrida terminou!");	
-		//Thread t2 = new Thread(fofa);
-		//t1.start();
-		//t2.start();
+
 	}
 
 }
--- Lebre.java
+++ Lebre.java
@@ -1,33 +1,48 @@
 package lebres;
 
-public class Lebre implements Runnable{
+public class Lebre {
 
-	private String nome;
-	private int tempo;
-
-	public Lebre(String nome, int tempo) {
-		this.nome = nome;
-		this.tempo = tempo;
-		//Thread t = new Thread(this);
-		//t.start();
-		//start();
-	}
-
-	@Override
-	public void run() {
-		System.out.println( nome + " foi iniciada");	
-
+	private boolean proximaLebre;
+	
+	
+	synchronized void lebre1(boolean checarCorrida) {
+		if(!checarCorrida) {
+			proximaLebre = true;
+			notify();
+			return;
+		}
+		System.out.println("Lebre 1 está correndo");
+		proximaLebre = true;
+		notify();
+		
 		try {
-			for(int i = 0; i <= 5; i++) {
-				System.out.println(nome + " contador: " + i);
-				Thread.sleep(tempo);
+			while(proximaLebre) {
+				wait();
 			}
 		}
-		catch(InterruptedException e) {
-			System.out.println("A lebre " + nome + "foi interrompida");
+		catch(InterruptedException e){
+			e.printStackTrace();
 		}
-
-		System.out.println( nome + " terminou de correr");
+	}
+	
+	synchronized void lebre2(boolean checarCorrida) {
+		if(!checarCorrida) {
+			proximaLebre = false;
+			notify();
+			return;
+		}
+		System.out.println("Lebre 2 está correndo");
+		proximaLebre = false;
+		notify();
+		
+		try {
+			while(!proximaLebre) {
+				wait();
+			}
+		}
+		catch(InterruptedException e){
+			e.printStackTrace();
+		}
 	}
 
 }
